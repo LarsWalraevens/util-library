a1cdd2770bfb0729431629f23ef9cf501ff3c3d0
Lars <39798840+LarsWalraevens@users.noreply.github.com>
2023-05-16 17:59:21 +0200

    Create useContext-template.jsx

---
+++ useContext-template.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState } from "react";
+
+// null -> loading
+// false -> failed
+// x -> correct
+var initFILLINState = false; // initial state
+export const FILLINContext = createContext(initFILLINState);
+
+export const useFILLINContextState = (useReactState) => {
+    const [FILLINState, setFILLINState] = useContext(FILLINContext);
+    if (useReactState) {
+        return [FILLINState, setFILLINState]; // Expects to be in [-, -] format like useState
+    }
+    else if (useReactState === false) {
+        return FILLINState; // return state
+    } else {
+        return setFILLINState; // return setState
+    }
+}
+
+const FILLINContextProvider = ({ children }) => {
+    // provide general React hook useState
+    const [FILLINState, setFILLIN] = useState(initFILLINState);
+    return (<FILLINContext.Provider value={[FILLINState, setFILLIN]}>
+        {children}
+    </FILLINContext.Provider>)
+}
+
+export default FILLINContextProvider;
