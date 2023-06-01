a992b86d90538a3cb9b2289436bef04fed073d5e
Lars Walraevens <larswalraevens@live.be>
2023-06-01 22:33:32 +0200

    tweaks to readmes & store example

---
+++ tailwind/README.md
@@ -0,0 +1,93 @@
+# Tailwind CSS Setup
+To set up Tailwind CSS in your project, follow these steps:
+
+1. Install Dependencies
+```bash
+yarn add -D tailwindcss postcss autoprefixer
+```
+2. Create tailwind.config.js File
+Create a tailwind.config.js file in the root of your project with the following content:
+
+```javascript
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js,tsx,ts}",
+    "./components/**/*.{html,js,tsx,ts}"
+  ],
+  theme: {
+    colors: {
+      white: {
+        100: '#FFFFFF',
+        200: '#FBFBFB',
+        300: '#FCFCFC',
+        400: '#F4F4F4',
+        500: '#F6F6F6',
+        600: '#F9F9F9',
+        900: '#C8C8C8'
+      },
+      black: {
+        100: '#39393A',
+        200: '#262E37',
+        400: '#2A2A2D',
+        900: '#000000'
+      },
+      grey: {
+        100: '#B1B1B1',
+        200: '#8D8D8D',
+        300: '#666667',
+        400: '#39393A'
+      },
+      blue: {
+        100: '#E0EEFF',
+        400: '#3C8AE5'
+      },
+      green: {
+        100: '#D1E7DD',
+        400: '#00E2B8',
+         }
+    },
+    fontSize: {
+      sm: '0.9rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem'
+    },
+    extend: {
+      fontFamily: {
+        DarkerGrotesque: ['Darker Grotesque', 'sans-serif'],
+        Lexend: ['Lexend', 'sans-serif'],
+        Mako: ['Mako', 'sans-serif'],
+        Poppins: ['Poppins', 'sans-serif']
+      }
+    },
+    plugins: []
+  }
+};
+```
+Make sure to customize the theme and other configurations according to your project's needs.
+
+3. Create Tailwind CSS File
+Create a CSS file (e.g., tailwind-import.css) in your project's styles directory with the following content:
+
+```css
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import './base.css';
+@import 'tailwindcss/utilities';
+
+@layer base {
+    h1 {
+        @apply text-2xl;
+        @apply font-bold;
+    }
+}
+```
+You can import additional CSS files or customize the styles as needed.
--- zustand/exampleStore.js
+++ zustand/exampleStore.js
@@ -1,36 +1,53 @@
-// zustand is a great state manager
+// Zustand is a great state manager
 
-// when you created store, import the store in other file:
-//      import useMyStore from '../my-location/useMyStore.js'
-//      const myStore = useMyStore();
+// When you create a store, import the store in other files:
+// import useMyStore from '../my-location/useMyStore.js'
+// const myStore = useMyStore();
 
-// example store:
-import produce from "immer"; // package to easily change complex object-states, ...
+// Example store:
+import produce from "immer"; // Package to easily change complex object states
 import { create } from 'zustand';
+
 export const useUserStore = create((set) => ({
-    userData: false,
-    ip: null,
-    checkedRememberMe: false,
+    userData: false, // Initial state for userData
+    ip: null, // Initial state for ip
+    checkedRememberMe: false, // Initial state for checkedRememberMe
+
     replaceUserData(newData) {
-        set((state) => produce(state, draft => {
-            draft.userData = newData;
-        }))
+        // Action to replace userData with new data
+        set((state) =>
+            produce(state, (draft) => {
+                draft.userData = newData;
+            })
+        );
     },
+
     setData(key, val) {
-        set((state) => produce(state, draft => {
-            draft[key] = val;
-        }))
+        // Action to set specific data in the store
+        set((state) =>
+            produce(state, (draft) => {
+                draft[key] = val;
+            })
+        );
     },
+
     replaceIp(newIp) {
-        set((state) => produce(state, draft => {
-            draft.ip = newIp;
-        }))
+        // Action to replace the ip with a new value
+        set((state) =>
+            produce(state, (draft) => {
+                draft.ip = newIp;
+            })
+        );
     },
+
     changeUserLanguage(props) {
-        set((state) => produce(state, draft => {
-            draft.userData.language.isoCode = props.isoCode;
-            draft.userData.language.id = props.id;
-            draft.userData.language.name = props.name;
-        }))
-    }
+        // Action to change the user's language
+        set((state) =>
+            produce(state, (draft) => {
+                draft.userData.language.isoCode = props.isoCode;
+                draft.userData.language.id = props.id;
+                draft.userData.language.name = props.name;
+            })
+        );
+    },
 }));
