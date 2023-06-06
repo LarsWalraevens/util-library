17644166872f55fdedff88a65fe85c5b1fd2c2f2
Lars <larswalraevens@live.be>
2023-06-06 12:00:41 +0200

    replace with immer

--- react-dnd/dndProvider.js
+++ react-dnd/dndProvider.js
@@ -2,21 +2,20 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { useCallback } from 'react';
-import update from 'immutability-helper';
+import produce from 'immer';
+
 
 // A component where you have a list and want to sort it by dragging them in the right order
 export default function MyDnDComponent() {
     const [dragItems, setDragItems] = useState([])
 
     const moveCard = useCallback((dragIndex, hoverIndex) => {
-        setDragItems((prevdragItems) =>
-            update(prevdragItems, {
-                $splice: [
-                    [dragIndex, 1],
-                    [hoverIndex, 0, prevdragItems[dragIndex]],
-                ],
-            }),
-        )
+        setDragItems(prevdragItems =>
+            produce(prevdragItems, draft => {
+                const [removed] = draft.splice(dragIndex, 1);
+                draft.splice(hoverIndex, 0, removed);
+            })
+        );
     }, []);
 
     return <div>
