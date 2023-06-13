41ff3b94c0209991cda140cf8ebfc1e89653070e
Lars <larswalraevens@live.be>
2023-06-13 17:55:17 +0200

    Added example prefetch (with react-query) from nextjs

---
+++ nextjs/prefetch-react-query-data.jsx
@@ -0,0 +1,41 @@
+import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+// When you want a page to have data from a HTTP call before it is loaded, then you can use nextjs prefetch functionality
+// This can work in combination with react-query by attaching the fetch to the queryKey along with react-query prefetchQuery functionality.
+// Here - you will cache the result in the queryKey and when you use it in your component it will show the cached data
+
+const fetchPokemon = () =>
+    axios
+        .get(`https://pokeapi.co/api/v2/pokemon`)
+        .then(({ data }) => data);
+
+export default function PokemonPage() {
+
+    const { data, isLoading, isError } = useQuery(
+        ["getPokemon"],
+        () => fetchPokemon()
+    );
+    console.warn(pokemon)
+
+    return (
+        <div>
+            {
+                isLoading ? "Loading" :
+                    isError ? "Error" :
+                        `Data loaded ${JSON.parse(data)}`
+            }
+        </div>
+    );
+}
+
+export const getStaticProps = async (ctx) => {
+    const queryClient = new QueryClient();
+    await queryClient.prefetchQuery(["getPokemon"], () => fetchPokemon());
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient)
+        },
+    };
+};
